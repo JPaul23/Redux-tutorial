fd04705a148df12c42f7bab0dc252f47c34d3013
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-08 23:41:29 +0200

    Action Types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import store from "./store";
+import * as actions from "./actionTypes";
 
 /* UI subscribes to store to get notified when the state changes and re-render*/
 const unsubscribe = store.subscribe(() => {
@@ -7,7 +8,7 @@ const unsubscribe = store.subscribe(() => {
 
 //call the store and dispatch actions ->adding a bug
 store.dispatch({
-  type: "bugAdded",
+  type: actions.BUG_ADDED,
   payload: {
     description: "Bug1",
   },
@@ -18,7 +19,7 @@ unsubscribe();
 
 //->adding a bug
 store.dispatch({
-  type: "bugRemoved",
+  type: actions.BUG_REMOVED,
   payload: {
     id: 1,
   },
--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+import * as actions from "./actionTypes";
+
 //[]
 
 let lastId = 0;
@@ -5,7 +7,7 @@ let lastId = 0;
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case "bugAdded":
+    case actions.BUG_ADDED:
       return [
         ...state,
         {
@@ -15,7 +17,7 @@ export default function reducer(state = [], action) {
         },
       ];
 
-    case "bugRemoved":
+    case actions.BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id);
 
     default:
