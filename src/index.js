19f94e5f946f64e02eabc237b23a94841e830c4a
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-08 23:15:10 +0200

    Subscribing to the Store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import store from "./store";
 
+/* UI subscribes to store to get notified when the state changes and re-render*/
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
+
 //call the store and dispatch actions ->adding a bug
 store.dispatch({
   type: "bugAdded",
@@ -7,6 +12,9 @@ store.dispatch({
     description: "Bug1",
   },
 });
+
+//getting unsubsribe
+unsubscribe();
 
 //->adding a bug
 store.dispatch({
