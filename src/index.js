1be8c2360211262f3d8fac513ef6264c0f12e7b4
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-08 19:19:40 +0200

    composing and Piping

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+
+let input = " JavaScript  ";
+
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
+
+const transform = compose(wrapInDiv, toLowerCase, trim); //this removes the wraping of function in other fns. order from right to left
+transform(input);
+const transform2 = pipe(trim, toLowerCase, wrapInDiv); //order from left to right
+transform2(input);
